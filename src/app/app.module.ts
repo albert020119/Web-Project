5be4c371021f105a94a3495aff6fcd44a839b7da
Albert Endre <albert_endre@yahoo.com>
2022-10-10 00:53:31 +0300

    added service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { AngularFireModule } from '@angular/fire/compat'
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,17 @@ import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDgHvFAqAImbQh9IFYPJWu8rE9iK3oV95s",
+      authDomain: "project-wp-f71f3.firebaseapp.com",
+      projectId: "project-wp-f71f3",
+      storageBucket: "project-wp-f71f3.appspot.com",
+      messagingSenderId: "123318577699",
+      appId: "1:123318577699:web:ca07b1e03502d7306c9f96"
+    })
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
